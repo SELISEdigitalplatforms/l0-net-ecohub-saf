b301c38fc44727059d91c05adf3ed2a191817677
Mohammad Mahmudul Hasan <mohammad.mahmudul@selisegroup.com>
2025-05-04 18:36:06 +0600

    Added service for verifying decrypted Public Key

--- src/SeliseBlocks.Ecohub.Saf/Helpers/SafDriverConstant.cs
+++ src/SeliseBlocks.Ecohub.Saf/Helpers/SafDriverConstant.cs
@@ -7,6 +7,7 @@ public static class SafDriverConstant
     public const string UploadMemberPublicKeyEndpoint = "publickeystore/keys";
     public const string GetMemberPublicKeyEndpoint = "publickeystore/members/{idpNumber}/key";
     public const string GetEncryptedPublicKeyEndpoint = "publickeystore/keys/{keyId}/getVerify";
+    public const string VerifyDecryptedPublicKeyEndpoint = "publickeystore/keys/{keyId}/postVerify";
     public const string SendOfferNlpiEventEndpoint = "saf/in";
     public const string ReceiveOfferNlpiEventEndpoint = "saf/{ecohubId}/offer/nlpi/out";
 }
