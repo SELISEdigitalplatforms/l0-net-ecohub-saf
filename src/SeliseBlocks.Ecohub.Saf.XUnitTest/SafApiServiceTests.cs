dd3547e97abec6e510607461f8796edee1c1d662
Mohammad Mahmudul Hasan <mohammad.mahmudul@selisegroup.com>
2025-05-04 17:30:00 +0600

    Added unit tests for UploadMemberPublicKey

--- src/SeliseBlocks.Ecohub.Saf.XUnitTest/SafApiServiceTests.cs
+++ src/SeliseBlocks.Ecohub.Saf.XUnitTest/SafApiServiceTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Moq;
+using SeliseBlocks.Ecohub.Saf.Helpers;
 using SeliseBlocks.Ecohub.Saf.Services;
 using Xunit;
 
@@ -69,5 +70,79 @@ public class SafApiServiceTests
 
         // Act & Assert
         await Assert.ThrowsAsync<Exception>(() => _safApiService.GetMemberPublicKey(bearerToken, idpNumber));
+    }
+
+    [Fact]
+    public async Task UploadMemberPublicKey_ShouldReturnResponse_WhenRequestIsSuccessful()
+    {
+        // Arrange  
+        var request = new SafMemberPublicKeyUploadRequest
+        {
+            BearerToken = "test-bearer-token",
+            Payload = new SafMemberPublicKeyUploadRequestPayload
+            {
+                Key = "public-key",
+                Version = "1.0",
+                ExpireInDays = "7"
+            }
+        };
+
+        var expectedResponse = new SafMemberPublicKeyResponse
+        {
+            KeyId = "key-id",
+            MembershipId = "membership-id",
+            Version = "1.0",
+            Key = "public-key",
+            CreatedAt = DateTime.UtcNow,
+            LastUpdatedAt = DateTime.UtcNow,
+            ActivatedAt = DateTime.UtcNow,
+            ExpiryDate = DateTime.UtcNow.AddYears(1),
+            EcoHubStatus = "Created"
+        };
+
+        _httpRequestGatewayMock
+            .Setup(x => x.PostAsync<SafMemberPublicKeyUploadRequest, SafMemberPublicKeyResponse>(
+                SafDriverConstant.UploadMemberPublicKeyEndpoint,
+                request,
+                null, // headers  
+                request.BearerToken,
+                "application/json")) // contentType  
+            .ReturnsAsync(expectedResponse);
+
+        // Act  
+        var result = await _safApiService.UploadMemberPublicKey(request);
+
+        // Assert  
+        Assert.NotNull(result);
+        Assert.Equal("key-id", result.KeyId);
+        Assert.Equal("public-key", result.Key);
+    }
+
+    [Fact]
+    public async Task UploadMemberPublicKey_ShouldThrowException_WhenRequestFails()
+    {
+        // Arrange
+        var request = new SafMemberPublicKeyUploadRequest
+        {
+            BearerToken = "test-bearer-token",
+            Payload = new SafMemberPublicKeyUploadRequestPayload
+            {
+                Key = "public-key",
+                Version = "1.0",
+                ExpireInDays = "14"
+            }
+        };
+
+        _httpRequestGatewayMock
+            .Setup(x => x.PostAsync<SafMemberPublicKeyUploadRequest, SafMemberPublicKeyResponse>(
+                SafDriverConstant.UploadMemberPublicKeyEndpoint,
+                request,
+                null,
+                request.BearerToken, 
+                "application/json"))
+            .ThrowsAsync(new Exception("Request failed"));
+
+        // Act & Assert
+        await Assert.ThrowsAsync<Exception>(() => _safApiService.UploadMemberPublicKey(request));
     }
 }
