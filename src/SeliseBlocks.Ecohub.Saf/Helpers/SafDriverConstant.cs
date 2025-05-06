3faa88fe65ab3bf2e9e153009239649c3778c728
Mohammad Mahmudul Hasan <mohammad.mahmudul@selisegroup.com>
2025-05-06 16:24:30 +0600

    Corrected url

--- src/SeliseBlocks.Ecohub.Saf/Helpers/SafDriverConstant.cs
+++ src/SeliseBlocks.Ecohub.Saf/Helpers/SafDriverConstant.cs
@@ -6,8 +6,8 @@ public static class SafDriverConstant
     public const string TechUserEnrolmentEndpoint = "general/techUserEnrolment";
     public const string UploadMemberPublicKeyEndpoint = "publickeystore/keys";
     public const string GetMemberPublicKeyEndpoint = "publickeystore/members/{idpNumber}/key";
-    public const string GetEncryptedPublicKeyEndpoint = "publickeystore/keys/{keyId}/getVerify";
-    public const string VerifyDecryptedPublicKeyEndpoint = "publickeystore/keys/{keyId}/postVerify";
+    public const string GetEncryptedPublicKeyEndpoint = "publickeystore/keys/{keyId}/verify";
+    public const string VerifyDecryptedPublicKeyEndpoint = "publickeystore/keys/{keyId}/verify";
     public const string ActivatePublicKeyEndpoint = "publickeystore/keys/{keyId}/activate";
     public const string SendOfferNlpiEventEndpoint = "saf/in";
     public const string ReceiveOfferNlpiEventEndpoint = "saf/{ecohubId}/offer/nlpi/out";
