9919b55392a92714ab4b1bd7a31916a06dcf3132
Mohammad Mahmudul Hasan <mohammad.mahmudul@selisegroup.com>
2025-05-16 15:04:56 +0600

    Added unit tests for Kafka event services

---
+++ src/SeliseBlocks.Ecohub.Saf.XUnitTest/SafKafkaEventServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using SeliseBlocks.Ecohub.Saf.Services;
+using SeliseBlocks.Ecohub.Saf;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Runtime.InteropServices.JavaScript;
+
+public class SafKafkaEventServiceTests
+{
+    [Fact]
+    public async Task ProduceEventAsync_ReturnsFalse_OnException()
+    {
+        // Arrange
+        var service = new SafKafkaEventService();
+        var request = new SafProduceKafkaEventRequest
+        {
+            KafkaServer = null, // This will cause an exception
+            KafkaProducerTopic = "test-topic",
+            TechUserCertificate = "invalid-base64", // Invalid certificate
+            TechUserPassword = "password",
+            SchemaRegistryUrl = "http://localhost:8081",
+            SchemaRegistryAuth = "user:pass",
+            EventPayload = new SafOfferNlpiEvent { Data = new SafData() }
+        };
+
+        // Act
+        var result = await service.ProduceEventAsync(request);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ConsumeEventAsync_ReturnsNull_OnException()
+    {
+        // Arrange
+        var service = new SafKafkaEventService();
+        var request = new SafConsumeKafkaEventRequest
+        {
+            KafkaServer = null, // This will cause an exception
+            TechUserCertificate = "invalid-base64",
+            TechUserPassword = "password",
+            GroupId = "group1",
+            EcohubId = "ecohub1"
+        };
+
+        // Act
+        var result = service.ConsumeEventAsync(request);
+
+        // Assert
+        Assert.Null(result);
+    }
+}
